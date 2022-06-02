1e1cdf29cf3895038938f8a69dbf76af97710f09
Christian Kunis <engineering@lixiehammers.com>
2022-06-02 14:08:37 -0400

    Added package management scripts

---
+++ scripts/PackageManagement.ps1
@@ -0,0 +1,16 @@
+#--- Unofficial Chocolatey Tools ---
+choco install -y choco-package-list-backup
+choco install -y choco-upgrade-all-at
+
+#--- Winget Automatic Scheduled Updates ---
+$STAction = New-ScheduledTaskAction -Execute 'winget' -Argument 'upgrade --all --accept-source-agreements'
+$STTrigger = New-ScheduledTaskTrigger -Daily -At 4am
+$STPrin = New-ScheduledTaskPrincipal -UserId 'NT AUTHORITY\SYSTEM' -RunLevel Highest
+$STSetings = New-ScheduledTaskSettingsSet
+
+if (Get-ScheduledTask -TaskName 'WingetUpgradeAllTask' -ErrorAction SilentlyContinue) {
+	Set-ScheduledTask -TaskName 'WingetUpgradeAllTask' -Action $STAction -Principal $STPrin -Settings $STSetings -Trigger $STTrigger
+} else {
+	Register-ScheduledTask -TaskName 'WingetUpgradeAllTask' -Action $STAction -Principal $STPrin -Settings $STSetings -Trigger $STTrigger
+}
+Clear-Variable STAction, STPrin, STSetings, STTrigger
