059cad3c5b57ba088af804f4db54765db9b31151
Christian Kunis <engineering@lixiehammers.com>
2022-06-02 14:31:07 -0400

    Added office tools script

---
+++ scripts/OfficeTools.ps1
@@ -0,0 +1,10 @@
+#--- PDF ---
+choco install -y adobereader
+# choco install -y foxitreader
+# choco install -y pdfsam
+
+#--- Office Suite ---
+choco install -y openoffice
+# choco install -y libreoffice-fresh
+# choco install -y onlyoffice
+choco install -y thunderbird
