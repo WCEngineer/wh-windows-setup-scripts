148f202962eb88299f41755be03c505adfd091ce
Christian Kunis <engineering@lixiehammers.com>
2022-06-02 14:47:42 -0400

    Added batch files for easy execution without navigating to web browser

---
+++ wh_setup_engineer.bat
@@ -0,0 +1,3 @@
+ECHO OFF
+START microsoft-edge:http://boxstarter.org/package/url?https://raw.githubusercontent.com/WCEngineer/wh-windows-setup-scripts/master/wh_engineer.ps1
+PAUSE
---
+++ wh_setup_workstation.bat
@@ -0,0 +1,3 @@
+ECHO OFF
+START microsoft-edge:http://boxstarter.org/package/url?https://raw.githubusercontent.com/WCEngineer/wh-windows-setup-scripts/master/wh_workstation.ps1
+PAUSE
