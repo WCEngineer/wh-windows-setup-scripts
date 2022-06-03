23c967292bda398fcd022037e8b8b6c7af784844
Christian Kunis <106686916+WCEngineer@users.noreply.github.com>
2022-06-03 14:12:51 -0400

    Comment out additional unneeded file

--- scripts/FileAndStorageUtils.ps1
+++ scripts/FileAndStorageUtils.ps1
@@ -4,8 +4,8 @@ choco install -y 7zip
 # choco install -y filebot
 # choco install -y Folder_Size
 choco install -y freefilesync
-choco install -y partitionwizard
-choco install -y rufus
+# choco install -y partitionwizard
+# choco install -y rufus
 # choco install -y teracopy
 choco install -y windirstat
 choco install -y winmerge
