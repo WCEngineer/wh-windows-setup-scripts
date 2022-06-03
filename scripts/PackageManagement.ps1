7e6dd41cd4149897b2927e533fb2898e71745bec
Christian Kunis <106686916+WCEngineer@users.noreply.github.com>
2022-06-03 13:08:20 -0400

    Comment out package list backup
    package list backup repeats after each reboot, and it slows down resuming the install after the reboot

--- scripts/PackageManagement.ps1
+++ scripts/PackageManagement.ps1
@@ -1,5 +1,5 @@
 #--- Unofficial Chocolatey Tools ---
-choco install -y choco-package-list-backup
+# choco install -y choco-package-list-backup
 choco install -y choco-upgrade-all-at
 
 #--- Winget Automatic Scheduled Updates ---
