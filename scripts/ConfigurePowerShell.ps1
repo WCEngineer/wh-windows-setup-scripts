06773c0099d6677f4accab44591b5f667d396439
Christian Kunis <ckunis98@gmail.com>
2022-06-04 11:06:14 -0400

    Remove unneeded powershell configuration options

--- scripts/ConfigurePowerShell.ps1
+++ scripts/ConfigurePowerShell.ps1
@@ -1,22 +1,3 @@
-Function Install-WinGetApp {
-	param([Parameter(Mandatory = $true, Position = 0)][string]$Id,
-		[Parameter(Mandatory = $false, Position = 1)][string]$Source)
-	#check if the app is already installed
-	$listApp = winget list --exact -q $Id
-	if (![String]::Join("", $listApp).Contains($Id)) {
-		Write-Host "Installing:" $Id
-		if ($Source -ne $null) {
-			winget install --exact --silent $Id --source $Source --accept-package-agreements --accept-source-agreements
-		} else {
-			winget install --exact --silent $Id --accept-package-agreements --accept-source-agreements
-		}
-		RefreshEnv;
-	} else {
-		Write-Host "Skipping Install of " $Id
-	}
-	Start-Sleep -Seconds 1;
-}
-
 if (([Security.Principal.WindowsPrincipal] `
 			[Security.Principal.WindowsIdentity]::GetCurrent() `
 	).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
@@ -24,13 +5,6 @@ if (([Security.Principal.WindowsPrincipal] `
 	choco upgrade -y powershell
 	choco upgrade -y powershell-core
 	refreshenv
-
-	#--- Windows Terminal ---
-	# Install-WinGetApp -Id 'Microsoft.WindowsTerminal' -Source 'msstore'
-
-	##--- Oh My Posh Environment Variable ---
-	#[System.Environment]::SetEnvironmentVariable('POSH_THEMES_PATH', '~\AppData\Local\Programs\oh-my-posh\themes')
-	#refreshenv
 }
 
 #--- Enable Powershell Script Execution
@@ -57,37 +31,6 @@ refreshenv
 		Write-Output $ProfileString | Write-Debug
 		Set-Content -Path $PROFILE -Value ($ProfileString, (Get-Content $PROFILE))
 	}
-
-	# #--- Install & Configure the Powerline Modules
-	# try {
-	# 	if (([Security.Principal.WindowsPrincipal] `
-	# 				[Security.Principal.WindowsIdentity]::GetCurrent() `
-	# 		).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-	# 		Write-Host 'Installing Oh-My-Posh - [Dependencies for Powerline]'
-	# 		winget install --id=JanDeDobbeleer.OhMyPosh --exact --silent --accept-package-agreements --accept-source-agreements
-	# 		refreshenv
-	# 	}
-	# 	[System.Environment]::SetEnvironmentVariable('POSH_THEMES_PATH', '~\AppData\Local\Programs\oh-my-posh\themes')
-	# 	refreshenv
-	# 	Write-Host 'Appending Configuration for Powerline to PowerShell Profile...'
-	# 	$PowerlineProfile = @(
-	# 		'# Dependencies for powerline',
-	# 		'oh-my-posh --init --shell pwsh --config $env:POSH_THEMES_PATH/microverse-power.omp.json | Invoke-Expression'
-	# 	)
-	# 	if (-not(Select-String -Pattern $PowerlineProfile[0] -Path $PROFILE )) {
-	# 		Write-Output 'Attempting to add the following lines to $PROFILE :' | Write-Debug
-	# 		Write-Output $PowerlineProfile | Write-Debug
-	# 		Add-Content -Path $PROFILE -Value $PowerlineProfile
-	# 	}
-	# 	# Install additional Powerline-related packages via chocolatey
-	# 	# choco install -y poshgit
-	# 	# choco install -y posh-github
-	# 	# refreshenv
-	# } catch {
-	# 	Write-Host  'Powerline failed to install' | Write-Warning
-	# 	Write-Host ' See the log for details (' $Boxstarter.Log ').' | Write-Debug
-	# 	# Move on if Powerline install fails due to error
-	# }
 
 	#--- Install & Configure the PSReadline Module
 	try {
@@ -157,19 +100,19 @@ refreshenv
 	# 	Add-Content -Path $PROFILE -Value $BoxstarterProfile
 	# }
 
-	#--- Install the Pipeworks Module
-	try {
-		Write-Host 'Installing Pipeworks -- [CLI Tools for PowerShell]'
-		Write-Host 'Description: PowerShell Pipeworks is a framework for writing Sites and Software Services in Windows PowerShell modules.'
-		if (-not(Get-Module -ListAvailable -Name Pipeworks)) {
-			Install-Module -Name Pipeworks -Scope CurrentUser -AllowClobber -SkipPublisherCheck -Force -Verbose
-		} else { Write-Host "Module 'Pipeworks' already installed" }
-		refreshenv
-	} catch {
-		Write-Host 'Pipeworks failed to install' | Write-Warning
-		Write-Host ' See the log for details (' $Boxstarter.Log ').' | Write-Debug
-		# Move on if Pipeworks install fails due to errors
-	}
+	# #--- Install the Pipeworks Module
+	# try {
+	# 	Write-Host 'Installing Pipeworks -- [CLI Tools for PowerShell]'
+	# 	Write-Host 'Description: PowerShell Pipeworks is a framework for writing Sites and Software Services in Windows PowerShell modules.'
+	# 	if (-not(Get-Module -ListAvailable -Name Pipeworks)) {
+	# 		Install-Module -Name Pipeworks -Scope CurrentUser -AllowClobber -SkipPublisherCheck -Force -Verbose
+	# 	} else { Write-Host "Module 'Pipeworks' already installed" }
+	# 	refreshenv
+	# } catch {
+	# 	Write-Host 'Pipeworks failed to install' | Write-Warning
+	# 	Write-Host ' See the log for details (' $Boxstarter.Log ').' | Write-Debug
+	# 	# Move on if Pipeworks install fails due to errors
+	# }
 
 	#--- Install the CredentialManager Module
 	try {
@@ -195,21 +138,3 @@ powershell -Command $ScriptBlock
 
 # Run the script block in PowerShell Core
 pwsh -Command $ScriptBlock
-
-# $WindowsTerminalSettingsDir = (Join-Path $env:LOCALAPPDATA '\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState')
-# $SymLinkPath = (Join-Path $env:USERPROFILE 'WindowsTerminalSettings')
-# if (Test-Path $WindowsTerminalSettingsDir) {
-# 	if ((-not(Test-Path $SymLinkPath)) -or (-not(Get-Item $SymLinkPath | Where-Object Attributes -Match ReparsePoint))) {
-# 		New-Item -Path $SymLinkPath -ItemType SymbolicLink -Value $WindowsTerminalSettingsDir -Force -Verbose
-# 	}
-# 	$RemoteBackup = '\\files.nerdygriffin.net\backup\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\.git'
-# 	if ((Test-Path $RemoteBackup) -and (-not(Test-Path (Join-Path $WindowsTerminalSettingsDir '.git')))) {
-# 		$PrevLocation = Get-Location
-# 		Set-Location -Path $RemoteBackup
-# 		git fetch; git pull;
-# 		Copy-Item -Path $RemoteBackup -Destination (Join-Path $WindowsTerminalSettingsDir '.git')
-# 		Set-Location -Path $PrevLocation
-# 	}
-# }
-
-# [System.Environment]::SetEnvironmentVariable('PYTHONSTARTUP', (Join-Path $env:USERPROFILE '.pystartup'))
