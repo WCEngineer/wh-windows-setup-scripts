ae5bd09aa933d92dcea1a4d8d612a1780f206764
Christian Kunis <ckunis98@gmail.com>
2022-06-04 11:17:07 -0400

    Remove winget helper function (no longer needed)

--- scripts/ConfigureGit.ps1
+++ scripts/ConfigureGit.ps1
@@ -1,22 +1,3 @@
-Function Install-WinGetApp {
-	param([Parameter(Mandatory = $true, Position = 0)][string]$Id,
-		[Parameter(Mandatory = $false, Position = 1)][string]$Source)
-	#check if the app is already installed
-	$listApp = winget list --exact -q $Id
-	if (![String]::Join("", $listApp).Contains($Id)) {
-		Write-Host "Installing:" $Id
-		if ($Source -ne $null) {
-			winget install --exact --silent $Id --source $Source --accept-package-agreements --accept-source-agreements
-		} else {
-			winget install --exact --silent $Id --accept-package-agreements --accept-source-agreements
-		}
-		RefreshEnv;
-	} else {
-		Write-Host "Skipping Install of " $Id
-	}
-	Start-Sleep -Seconds 1;
-}
-
 if (([Security.Principal.WindowsPrincipal] `
 			[Security.Principal.WindowsIdentity]::GetCurrent() `
 	).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
