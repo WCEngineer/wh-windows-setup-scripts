c6eb319a5fad6f8f658db0842ada4e1b18de090b
Christian Kunis <ckunis98@gmail.com>
2022-06-04 11:18:27 -0400

    Remove unused packages

--- scripts/FileAndStorageUtils.ps1
+++ scripts/FileAndStorageUtils.ps1
@@ -1,12 +1,6 @@
 #--- File & Storage Utilities ---
 choco install -y 7zip
-# choco install -y etcher
-# choco install -y filebot
-# choco install -y Folder_Size
 choco install -y freefilesync
-# choco install -y partitionwizard
-# choco install -y rufus
-# choco install -y teracopy
 choco install -y windirstat
 choco install -y winmerge
 choco install -y winmerge-7z
