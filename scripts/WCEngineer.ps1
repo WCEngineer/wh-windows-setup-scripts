517a030f5dfb5a51f8b61a2902972c1645254f04
Christian Kunis <engineering@lixiehammers.com>
2023-04-27 07:57:00 -0400

    Re-add GIMP and Inkscape for R.H.

--- scripts/WCEngineer.ps1
+++ scripts/WCEngineer.ps1
@@ -1,5 +1,6 @@
 # choco install -y blender
-choco install -y dwgtrueview
-choco install -y edrawings-viewer
+# choco install -y dwgtrueview
+# choco install -y edrawings-viewer
 # choco install -y fritzing
-# choco install -y inkscape
+choco install -y gimp
+choco install -y inkscape
