dea7f1a547ee22f8db36ea69ac0d49a161d8c0ee
Christian Kunis <106686916+WCEngineer@users.noreply.github.com>
2023-06-06 10:54:48 -0400

    Comment out FreeFileSync

--- scripts/FileAndStorageUtils.ps1
+++ scripts/FileAndStorageUtils.ps1
@@ -1,6 +1,6 @@
 #--- File & Storage Utilities ---
 choco install -y 7zip
-choco install -y freefilesync
+#choco install -y freefilesync
 choco install -y windirstat
 choco install -y winmerge
 choco install -y winmerge-7z
