63a0088c9be63e30b9d2b3e81bcbdbb5e7b5db25
Christian Kunis <106686916+WCEngineer@users.noreply.github.com>
2023-06-06 14:00:13 -0400

    Remove Winget attempt to install Microsoft office

--- scripts/OfficeTools.ps1
+++ scripts/OfficeTools.ps1
@@ -4,7 +4,6 @@ choco install -y adobereader
 # choco install -y pdfsam
 
 #--- Office Suite ---
-winget install --id='9WZDNCRD29V9' --exact --silent --accept-package-agreements --accept-source-agreements # Microsoft 365 (Office)
 # choco install -y openoffice
 # choco install -y libreoffice-fresh
 # choco install -y onlyoffice
