7253bbee9affda84122b82c1873bf977b4ff0ab2
Christian Kunis <engineering@lixiehammers.com>
2023-06-09 09:16:01 -0400

    Update URL for local nexus repo

--- scripts/ConfigureChocolatey.ps1
+++ scripts/ConfigureChocolatey.ps1
@@ -1,4 +1,4 @@
 Write-Host 'Configuring Chocolatey to use local Nexus repository'
 # choco source disable --name='chocolatey'
-choco source add --name='nexus' --source='http://192.168.1.200:8081/repository/chocolatey-group/' --priority=0
+choco source add --name='nexus' --source='http://nexus.whc.lan:8081/repository/chocolatey-group/' --priority=0
 choco config set --name="commandExecutionTimeoutSeconds" --value="14400"
