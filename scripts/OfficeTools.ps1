3b4f7f95ceb4a43bdab2bfbd59dd8c1cc6e591e0
Christian Kunis <ckunis98@gmail.com>
2025-03-15 11:09:57 -0400

    cleanup OfficeTools.ps1

--- scripts/OfficeTools.ps1
+++ scripts/OfficeTools.ps1
@@ -1,11 +1,6 @@
 #--- PDF ---
 choco install -y adobereader
-# choco install -y foxitreader
 # choco install -y pdfsam
 
 #--- Office Suite ---
-# choco install -y openoffice
-# choco install -y libreoffice-fresh
 winget install --id=ONLYOFFICE.DesktopEditors --exact --silent --accept-package-agreements --accept-source-agreements
-# choco install -y onlyoffice
-# choco install -y thunderbird
