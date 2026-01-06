df3ba89db51dbae129d037a5564a5fc954bf3afb
Christian Kunis <engineering@lixiehammers.com>
2026-01-06 15:07:02 -0500

    feat: implement winget installation for Google Chrome and Mozilla Firefox with fallback to Chocolatey

--- scripts/Browsers.ps1
+++ scripts/Browsers.ps1
@@ -1,3 +1,11 @@
 #--- Browsers ---
-choco install -y googlechrome
-choco install -y firefox
+try {
+	winget install --id=Google.Chrome --exact --silent --accept-package-agreements --accept-source-agreements
+} catch {
+	choco install -y googlechrome
+}
+try {
+	winget install --id=Mozilla.Firefox --exact --silent --accept-package-agreements --accept-source-agreements
+} catch {
+	choco install -y firefox
+}
