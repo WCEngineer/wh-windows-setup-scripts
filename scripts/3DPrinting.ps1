6e12cb13a22fcbe7c5b7024dfd2dcd767b36fef5
Christian Kunis <engineering@lixiehammers.com>
2026-01-06 15:04:02 -0500

    Replace Ultimaker.Cura installation with SoftFever.OrcaSlicer

--- scripts/3DPrinting.ps1
+++ scripts/3DPrinting.ps1
@@ -1,2 +1,2 @@
 winget install --id=OpenSCAD.OpenSCAD --exact --silent --accept-package-agreements --accept-source-agreements
-winget install --id=Ultimaker.Cura --exact --silent --accept-package-agreements --accept-source-agreements
+winget install --id=SoftFever.OrcaSlicer --exact --silent --accept-package-agreements --accept-source-agreements
