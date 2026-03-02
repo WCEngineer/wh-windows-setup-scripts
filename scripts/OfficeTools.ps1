3ab3d550c45b1100ec29228c029b10c4cb628887
Christian Kunis <engineering@lixiehammers.com>
2026-03-02 08:32:20 -0500

    Try new PDF editors

--- scripts/OfficeTools.ps1
+++ scripts/OfficeTools.ps1
@@ -1,6 +1,14 @@
 #--- PDF ---
-choco install -y adobereader
-# choco install -y pdfsam
+try {
+	winget install --id=PDFgear.PDFgear --exact --silent --accept-package-agreements --accept-source-agreements
+} catch {
+	choco install -y pdfgear
+}
+try {
+	winget install --id=geeksoftwareGmbH.PDF24Creator --exact --silent --accept-package-agreements --accept-source-agreements
+} catch {
+	choco install -y pdf24
+}
 
 #--- Office Suite ---
 winget install --id=ONLYOFFICE.DesktopEditors --exact --silent --accept-package-agreements --accept-source-agreements
